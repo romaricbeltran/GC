fba4492e3c1b93fad08282593fd58cc445911175
romaricbeltran <romaric.beltran@gmail.com>
2018-07-09 12:57:14 +0200

    Iteration 29 - Vue des Ecrits

--- Symfony/src/RB/EcritBundle/Controller/EcritController.php
+++ Symfony/src/RB/EcritBundle/Controller/EcritController.php
@@ -12,8 +12,19 @@ class EcritController extends Controller
     public function indexAction()
     {
     	$em=$this->getDoctrine()->getManager();
-    	$categorie = $em->getRepository('RBEcritBundle:Categorie')->findOneByNom('Ecrits Théoriques');
-    	var_dump($categorie);
-        return $this->render('RBEcritBundle:Ecrit:index.html.twig');
+
+        $catEcrits=$em->getRepository('RBEcritBundle:Categorie')->findOneByNom('Ecrits Théoriques');
+        $catPoemes=$em->getRepository('RBEcritBundle:Categorie')->findOneByNom('Poèmes');
+        $catRomans=$em->getRepository('RBEcritBundle:Categorie')->findOneByNom('Romans et Nouvelles');
+
+    	$ecrits = $em->getRepository('RBEcritBundle:Texte')->findBy(array('categorie' => $catEcrits));
+        $poemes = $em->getRepository('RBEcritBundle:Texte')->findBy(array('categorie' => $catPoemes));
+    	$romans = $em->getRepository('RBEcritBundle:Texte')->findBy(array('categorie' => $catRomans));
+
+        return $this->render('RBEcritBundle:Ecrit:index.html.twig', array(
+            'ecrits' => $ecrits,
+            'romans' => $romans,
+            'poemes' => $poemes,
+        ));
     }
 }
